56875a2606ddb0c36b3df2664ff2e2f58690efb1
LH <19070@my.sanctamaria.school.nz>
2024-09-11 15:57:36 +1200

    Allow GET+POST requests
    auth.py - for the login and signup routes to login.html and signup.html

--- website/auth.py
+++ website/auth.py
@@ -1,21 +1,33 @@
 # Store organised routes for authentication functions using a Blueprint
 # Import Blueprint and various modules + functions
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
+
 
 # Create and define Blueprint for auth variable
 auth = Blueprint("auth", __name__)
 
-# Register routes to auth Blueprint
-# Define authentication functions. Each will return the function, render_template, to render their respective HTML files from website/templates
-@auth.route("/login")
+
+# Register routes to auth Blueprint, allow for GET and POST requests
+# Define login function, receives data from login form and returns render_template for login.html
+@auth.route("/login", methods=['GET', 'POST'])
 def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    
     return render_template("login.html")
 
-@auth.route("/sign-up")
+# Define sign_up function, receives data from signup form and returns render_template for signup.html
+@auth.route("/sign-up", methods=['GET', 'POST'])
 def sign_up():
+    email = request.form.get("email")
+    username = request.form.get("username")
+    password1 = request.form.get("password1")
+    password2 = request.form.get("password")
+    
     return render_template("signup.html")
 
-# The function, logout, will return a redirect to the url for the home function in the views Blueprint
+
+# Define logout function, returns a redirect to url_for the home function in views.py
 @auth.route("/logout")
 def logout():
     return redirect(url_for("views.home"))
