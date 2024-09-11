fa5d683294da412e13143af8689875339df86e2c
LH <19070@my.sanctamaria.school.nz>
2024-09-11 17:40:08 +1200

    Register and Log in Users
    auth.py - login and signup routes

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,7 @@
 # Store organised routes for authentication functions using a Blueprint
 # Import Blueprint and various modules + functions
 from flask import Blueprint, render_template, redirect, url_for, request
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from .models import user
@@ -16,9 +16,23 @@ auth = Blueprint("auth", __name__)
 # ========== Login Route ==========
 @auth.route("/login", methods=['GET', 'POST'])
 def login():
+    # Retrieve POST data
     email = request.form.get("email")
     password = request.form.get("password")
     
+    # Check if user exists and if user password is correct, Login user within session and Redirect to home.html if so
+    # Create variable to check if user exists within database
+    user = User.query.filter_by(email=email).first()
+    if user: # If user exists and password is correct
+        if check_password_hash(user.password, password):
+            flash("Logged in successfully!", category='success')
+            login_user(user, remember=True)
+            return redirect(url_for('views.home'))
+        else: # If password is incorrect
+            flash('Incorrect password', category='error')
+    else: # If user does not exist
+        flash('No user account with this email exists.', category='error')
+        
     return render_template("login.html")
 
 # ========== Sign Up Route ==========
@@ -32,7 +46,7 @@ def sign_up():
         password1 = request.form.get("password1")
         password2 = request.form.get("password")
         
-        # Create User Account, Add to database, and Redirect to home.html if prerequisites are met
+        # Create user account, Add to database, Login user, and Redirect to home.html if prerequisites are met
         # Create variables to check if email and username exist in database
         email_exists = User.query.filter_by(email=email).first()
         username_exists = User.query.filter_by(username=username).first()
@@ -54,14 +68,17 @@ def sign_up():
             new_user = User(email=email, username=username, password=generate_password_hash(password1, method=sha256))
             db.session.add(new_user)
             db.session.commit()
+            login_user(new_user, remember=True)
             flash('Account created!')
             return redirect(url_for(views.home))
     
-    # Return signup.html if prerequisites are not met
+    # Return signup.html template if prerequisites are not met
     return render_template("signup.html")
 
 # ========== Logout Route ==========
 # Define logout function, returns a redirect to url_for the home function in views.py
 @auth.route("/logout")
+@login_required
 def logout():
+    logout_user
     return redirect(url_for("views.home"))
