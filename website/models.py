4fc272fb962f75f6b692d5b95705c73f5ce5176d
LH <19070@my.sanctamaria.school.nz>
2024-10-21 13:28:37 +1300

    Comments + Page Refresh + Misc.

--- website/models.py
+++ website/models.py
@@ -6,17 +6,12 @@ from sqlalchemy.sql import func
 
 # =============== User Model ===============
 '''
- Description
- Details
- Relationships
- 
  - Primary key = id
  - Users are automatically given a unique id on successful signup
  
- Email has a maximum character length of 320, it must be unique to prevent duplicate emails
-posts relationship reference all posts that the user has, any post with an author id = user id. 
-addition on user table referencing users posts. backreference allows to access user model. 
-passive delete allows all posts to be deleted when the user account is deleted.
+Email has a maximum character length of 320, it must be unique to prevent duplicate emails.
+Posts relationship reference all posts that the user has, any post with an author id = user id. 
+Passive delete allows all posts to be deleted when the user account is deleted.
 '''
 
 class User(db.Model, UserMixin):
@@ -31,12 +26,11 @@ class User(db.Model, UserMixin):
     
 # =============== Post Model ===============
 '''
-description appears on database table
-give post unique id
-require text and date_created
-know author by referncing from user model id to know user
-if user is deleted, the users posts are also deleted. through cascading on delete
-nullable false means must have a value. one to many relationship where one user has many posts.
+All posts are given a unique id
+They require text and date_created
+Knows post author by referencing user model relationship id
+If user is deleted, the users posts are also deleted. through cascading on delete
+One to many relationship where one user has many posts.
 '''
 
 class Post(db.Model):
@@ -49,7 +43,7 @@ class Post(db.Model):
     
 # ================= Like Model =================
 '''
-description
+Checks if current user account has clicked the 'like' button, then likes or unlikes the post.
 '''
 
 class Like(db.Model):
@@ -60,17 +54,16 @@ class Like(db.Model):
    
 # =============== Comment Model ===============
 '''
-description
-give comment unique id
-require text and date_created
-know author by referncing from user model id to know user
-if user is deleted, the users posts are also deleted. through cascading on delete
-nullable false means must have a value. one to many relationship where one user has many posts.
+All comments are given a unique id
+They require text and date_created
+Within 300 characters
+Asks for author of comment and post id that the comment replied to
+One to many relationship where one post has many comments
 '''
 
 class Comment(db.Model):
    id = db.Column(db.Integer, primary_key=True)
-   text = db.Column(db.Text, nullable=False)
+   text = db.Column(db.String(300), nullable=False)
    date_created = db.Column(db.DateTime(timezone=True), default=func.now())
    author = db.Column(db.Integer, db.ForeignKey('user.id',ondelete="CASCADE"), nullable=False)
    post_id = db.Column(db.Integer, db.ForeignKey('post.id',ondelete="CASCADE"), nullable=False)
--- website/views.py
+++ website/views.py
@@ -1,8 +1,8 @@
 # Store organised routes for core blog functions using a Blueprint
 # Import Blueprint, models, database, and various modules + functions
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 from flask_login import login_required, current_user
-from .models import Post, User
+from .models import Post, User, Comment, Like
 from . import db
 
 # Create and define Blueprint for views variable
@@ -10,7 +10,7 @@ views = Blueprint("views", __name__)
 
 # Register routes to views Blueprint
 
-# =============== Home Route and Function ===============
+# =============== Home & Blog Route and Function ===============
 ''' 
 The Home function has access to all posts, this is so they can be displayed within the posts page.
 Additionally, it will return the function, render_template, to redirect the user to the home.html
@@ -22,6 +22,12 @@ page.
 def home():
     posts = Post.query.all()
     return render_template("home.html", user=current_user, posts=posts)
+
+@views.route("/blog")
+@login_required
+def blog():
+   posts = Post.query.all()
+   return render_template("posts_div.html", user=current_user, posts=posts)
 
 # ============= Create Post Route and Function ============
 '''
@@ -49,9 +55,35 @@ def create_post():
             db.session.add(post)
             db.session.commit()
             flash('Post created!', category='success')
-            return redirect(url_for('views.home'))
+            return redirect(url_for('views.blog'))
 
     return render_template('create_post.html', user=current_user)
+
+# ============ Like Posts Route and Function =============
+'''
+The User Likes function has been allowed the POST method. The purpose of the function is to check
+whether a post exists and if it does, it will check if the user has liked the post. If the user
+has not liked the post, then it will like the post. If the user has liked the post, it will unlike
+the post. It is required that a user is logged into a user account to access. Javascript calls for this.
+'''
+@views.route("/like-post/<post_id>", methods=['POST'])
+@login_required
+def like(post_id):
+    post = Post.query.filter_by(id=post_id).first()
+    like = Like.query.filter_by(
+        author=current_user.id, post_id=post_id).first()
+
+    if not post:
+        return jsonify({'error': 'Post does not exist.'}, 400)
+    elif like:
+        db.session.delete(like)
+        db.session.commit()
+    else:
+        like = Like(author=current_user.id, post_id=post_id)
+        db.session.add(like)
+        db.session.commit()
+
+    return jsonify({"likes": len(post.likes), "liked": current_user.id in map(lambda x: x.author, post.likes)})
 
 # ============ User Posts Route and Function =============
 '''
@@ -60,8 +92,8 @@ user account's username. It will check if the user account exists and if it does
 the function will collect and display all posts made by the selected user account.
 
 Otherwise, if the user account does not exist, then the function will flash an error
-message and redirect the user back to the home page. It is required that a user is logged into a 
-user account to access the create post page.
+message and redirect the user back to the posts page. It is required that a user is logged into a 
+user account to access the user posts page.
 '''
 
 @views.route("/posts/<username>")
@@ -69,8 +101,8 @@ user account to access the create post page.
 def posts(username):
    user = User.query.filter_by(username=username).first()
    if not user:
-       flash('No user with that username exists.', category='error;')
-       return redirect(url_for('views.home'))
+       flash('No user with that username exists.', category='error')
+       return redirect(url_for('views.blog'))
    
    posts = user.posts
    return render_template("posts.html", user=current_user, posts=posts, username=username)
@@ -82,7 +114,7 @@ It will check the unique id given to the user post and check is the post exists.
 it will check if the user has the correct permissions to delete the post through the
 post author id and the user account id of the user. If these prerequisites are met,
 the post will be deleted from display and the database. It is required that a user 
-is logged into a user account to access the create post page.
+is logged into a user account to access the delete post page.
 
 Otherwise, if the post does not exist or the user does not have sufficient permissions,
 then the function will return an error flash message and return a redirect for the home
@@ -101,4 +133,54 @@ def delete_post(id):
        db.session.delete(post)
        db.session.commit()
        flash('Post deleted.', category='success')
-   return redirect(url_for('views.home'))
+   return redirect(url_for('views.blog'))
+
+# ============ Create Comment Route and Function =============
+'''
+The Create Comment function is allowed for the POST method, it displays a form to the 
+user account that, once filled in, will create a comment, store it into the database,
+and have it displayed under the post it was commented on. The user must be authenticated
+to comment under a post.
+
+Otherwise, if the prerequisites are not met, then the function will flash an error 
+message then redirect to the home page.
+'''
+
+@views.route("/create-comment/<post_id>", methods=['POST'])
+@login_required
+def create_comment(post_id):
+   text = request.form.get('text')
+   if not text:
+       flash('Comment cannot be empty.', category='error')
+   else:
+       post = Post.query.filter_by(id=post_id)
+       if post:
+           comment = Comment(text=text, author=current_user.id, post_id=post_id)
+           db.session.add(comment)
+           db.session.commit()
+       else:
+           flash('Post does not exist.', category='error')
+   return redirect(url_for('views.blog'))
+
+# ============ Delete Comment Route and Function =============
+'''
+The Delete Comment function checks if comment exists, if it does not, return an error. 
+If it does, check if the current user has the permissions to delete the comment. If 
+they do not, return an error flash message. If the user meets these two prerequisites,
+the comment will be deleted from display and the database.
+'''
+
+@views.route("/delete-comment/<comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.filter_by(id=comment_id).first()
+    if not comment:
+       flash('Comment does not exist.', category='error')    
+    elif current_user.id != comment.author and current_user.id != comment.post.author:
+       flash('You do not have permission to delete this comment.', category='error')
+    else:
+       db.session.delete(comment)
+       db.session.commit()
+       
+    return redirect(url_for('views.blog'))
+       
